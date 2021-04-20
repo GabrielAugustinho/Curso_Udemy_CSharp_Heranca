1c9814b3409f6f1ebfc96c510258605f1bfc3adc
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-04-20 17:50:07 -0300

    Adicionado a classe SavingsAccount, estudo de 'Upcasting' e 'Downcasting', além do método 'as' e 'is'

--- Heranca_relacao/Program.cs
+++ Heranca_relacao/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Heranca_relacao.Entities;
 
 namespace Heranca_relacao
 {
@@ -10,6 +7,34 @@ namespace Heranca_relacao
     {
         static void Main(string[] args)
         {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            // UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "João", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            // DOWNCASTING "Operaão insegura."
+
+            // Casting --> (BusinessAccount)acc2 --> acc2 as BusinessAccount
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            // Testar os tipos
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(100.0);
+                Console.WriteLine("Loan!");
+            }
+            else if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
