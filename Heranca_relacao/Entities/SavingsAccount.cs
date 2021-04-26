d731ef3bea5ee61d2d11ab6fe0f8ae46d41bfa76
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-04-26 13:52:29 -0300

    Adição de comentários para melhor organização

--- Heranca_relacao/Entities/SavingsAccount.cs
+++ Heranca_relacao/Entities/SavingsAccount.cs
@@ -5,7 +5,8 @@
         public double InterestRate { get; set; }
 
         public SavingsAccount() { }
-        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        public SavingsAccount(int number, string holder, double balance, double interestRate) 
+            : base(number, holder, balance)
         {
             InterestRate = interestRate;
         }
